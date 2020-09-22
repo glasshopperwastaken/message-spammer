1150de52956e644f855c291caa659e9a5233a889
DefyEnjoyCantCode <71650383+DefyEnjoyCantCode@users.noreply.github.com>
2020-09-22 16:56:14 -0400

    Another small adjustment

--- spammy.py
+++ spammy.py
@@ -1,9 +1,12 @@
 import time
 import pyautogui
 
+#The main script, make adjustments if you'd like.
+
 time.sleep(2)
 print("Spamming has begun, so prepare for epic trolling ehehe")
 f = open("beemovie.txt", 'r')
 for word in f:
     pyautogui.typewrite(word, 0.09)
     pyautogui.press('enter')
+    time.sleep(0.5)
