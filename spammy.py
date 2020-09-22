f95f775fededb5564d846c94697cbbf3f041d62e
DefyEnjoyCantCode <71650383+DefyEnjoyCantCode@users.noreply.github.com>
2020-09-22 16:46:57 -0400

    Adjusted script for the most realistic typing
    Changeable if you want

--- spammy.py
+++ spammy.py
@@ -1,9 +1,9 @@
 import time
 import pyautogui
 
-
 time.sleep(2)
+print("Spamming has begun, so prepare for epic trolling ehehe")
 f = open("beemovie.txt", 'r')
 for word in f:
-    pyautogui.typewrite(word, 0.01)
+    pyautogui.typewrite(word, 0.09)
     pyautogui.press('enter')
